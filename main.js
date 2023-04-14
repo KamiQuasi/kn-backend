1d7ef29b1c95a7b04e8ba3074985502bad7feca0
Luke Dary <lukedary@gmail.com>
2023-04-14 19:18:06 -0400

    adding method handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { serve } from "https://deno.land/std@0.178.0/http/server.ts";
 import { PathParser } from "./src/PathParser.js";
 const port = Deno.env.get('PORT') ?? '8080';
+const graph = Deno.env.get('CPX_GRAPH') ?? 'oxigraph:7878';
 const hostname = "0.0.0.0";
 
 const handler = (request) => {
@@ -9,8 +10,20 @@ const handler = (request) => {
   const status = 200;
   const headers = new Headers();
   headers.set('Content-Type','application/json');
-  asset.method = request.method;
-  return new Response(JSON.stringify(asset), {status,headers} );
+  let resp;
+
+  switch (request.method) {
+    case 'POST':
+      // curl -X POST -H 'Content-Type: application/sparql-update' --data 'DELETE WHERE { <http://example.com/s> ?p ?o }' http://localhost:7878/update
+      resp = new Response(JSON.stringify({result:'success'}), {status,headers});
+      break;
+    default:
+      // curl -X POST -H 'Content-Type:application/sparql-query' --data 'SELECT * WHERE { ?s ?p ?o } LIMIT 10' http://localhost:7878/query
+      // curl -X POST -H 'Content-Type:application/sparql-query' --data 'SELECT * WHERE { ?s ?p ?o } LIMIT 10' http://localhost:7878/query
+      resp = new Response(JSON.stringify({asset:asset,method:request.method}), {status,headers} );
+  }
+  
+  return resp;
 }
 
 await serve(handler, {hostname,port});
